60a35855098141ddbc647ef2b300bb09f4d9992c
Salma Elakhal <salma.elakhal.pro@gmail.com>
2025-07-06 13:42:07 +0200

    Added my project

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <h1 className="underline text-4xl font-bold">Welcome to the App</h1>
+    </div>
+  )
+}
+
+export default App
