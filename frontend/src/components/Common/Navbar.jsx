545f07951b64975b55ec421f7caa595e361e5282
Salma Elakhal <salma.elakhal.pro@gmail.com>
2025-07-09 04:03:18 +0200

    Add NavDrawer UI

--- frontend/src/components/Common/Navbar.jsx
+++ frontend/src/components/Common/Navbar.jsx
@@ -69,11 +69,20 @@ function Navbar() {
             <CartDrawer drawerOpen={drawerOpen} toggleCartDrawer={toggleCartDrawer} />
 
             {/* Mobile Navigation */}
-            <div className={`fixed top-0 left-0 w-3/4 sm:w-1/2 md:w-1/3 h-full bg-white shadow-lg transform transistion-transform duration-300 z-50 ${navDrawerOpen ? 'translate-x-0' : 'translate-x-full'}`}>
+            <div className={`fixed top-0 left-0 w-3/4 sm:w-1/2 md:w-1/3 h-full bg-white shadow-lg transform transistion-transform duration-300 z-50 ${navDrawerOpen ? 'translate-x-0' : '-translate-x-full'}`}>
                 <div class="flew justify-end p-4">
-                    <button>
+                    <button onClick={toggleNavDrawer}>
                         <IoMdClose className='h-6 w-6 text-gray-600'></IoMdClose>
                     </button>
+                </div>
+                <div className="p-4">
+                    <h2 className="text-xl font-semibold mb-4"></h2>
+                    <nav className="space-y-4">
+                        <Link to="#" onClick={toggleNavDrawer} className='block text-gray-600 hover:text-black'>Men</Link>
+                        <Link to="#" onClick={toggleNavDrawer} className='block text-gray-600 hover:text-black'>Women</Link>
+                        <Link to="#" onClick={toggleNavDrawer} className='block text-gray-600 hover:text-black'>Top Wear</Link>
+                        <Link to="#" onClick={toggleNavDrawer} className='block text-gray-600 hover:text-black'>Bottom Wear</Link>
+                    </nav>
                 </div>
             </div>
 
