d70f8829185b8a009e02624bd140fb8f8133211e
Salma Elakhal <salma.elakhal.pro@gmail.com>
2025-07-10 15:01:17 +0200

    Complete Explore New Arrivals UI

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Hero from '../components/Layout/Hero'
 import GenderCollectionSection from '../components/Products/GenderCollectionSection'
+import NewArrivals from '../components/Products/NewArrivals'
 
 function Home() {
   return (
       <div>
           <Hero />
           <GenderCollectionSection />
+          <NewArrivals/>
     </div>
   )
 }
