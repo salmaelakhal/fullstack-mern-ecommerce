c873f0d8b7f0c9dcd40591eee7c28f2060379927
Salma Elakhal <salma.elakhal.pro@gmail.com>
2025-07-10 20:40:19 +0200

    Complete Features Section UI

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -5,6 +5,7 @@ import NewArrivals from '../components/Products/NewArrivals'
 import ProductDetails from '../components/Products/ProductDetails'
 import ProductGrid from '../components/Products/ProductGrid'
 import FeaturedCollection from '../components/Products/FeaturedCollection'
+import FeaturesSection from '../components/Products/FeaturesSection'
 
 function Home() {
 
@@ -28,23 +29,12 @@ function Home() {
       images: [{ url: "https://picsum.photos/200?random=3", }]
     },
     {
-      _id: 1,
+      _id: 4,
       name: 'Product 4',
       price: 120,
       images: [{ url: "https://picsum.photos/200?random=4", }]
   },
-  {
-      _id: 2,
-      name: 'Product 5',
-      price: 120,
-      images: [{ url: "https://picsum.photos/200?random=5", }]
-  },
-  {
-      _id: 3,
-      name: 'Product 6',
-      price: 120,
-      images: [{ url: "https://picsum.photos/200?random=6", }]
-  },
+
   ]
 
 
@@ -64,6 +54,7 @@ function Home() {
         <ProductGrid products={placeholderProducts} />
       </div>
       <FeaturedCollection />
+      <FeaturesSection />
     </div>
   )
 }
