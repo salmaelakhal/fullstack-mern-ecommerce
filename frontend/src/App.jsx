934e1ddeeaa7383f00c4cab6eae46b3366bc93c0
Salma Elakhal <salma.elakhal.pro@gmail.com>
2025-07-12 12:56:34 +0200

    Complete Login & Register page UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,9 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import UserLayout from './components/Layout/UserLayout';
 import Home from './pages/Home';
 import { Toaster } from 'sonner';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Profile from './pages/Profile';
 function App() {
   return (
     <BrowserRouter>
@@ -10,6 +13,9 @@ function App() {
       <Routes>
         <Route path="/" element={<UserLayout />} >
           <Route index element={<Home />} />
+          <Route path='login' element={<Login />} />
+          <Route path='register' element={<Register />} />
+          <Route path='profile' element={<Profile />} />
       </Route>
       
       <Route> { /* Admin Layout*/} </Route>
