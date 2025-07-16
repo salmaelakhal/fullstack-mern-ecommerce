944a8c2615f1ededc8585cb11e8a4983779409d2
Salma Elakhal <salma.elakhal.pro@gmail.com>
2025-07-16 03:30:12 +0200

    Add product Update functionality with admin protection

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -63,4 +63,76 @@ router.post("/", protect,admin,  async (req, res) => {
   }
 });
 
+
+
+
+// @route Put /api/products/:id
+// @desc Update a Product
+// @access Private,  // only admin can update a product
+
+router.put("/:id", protect,admin, async (req, res) => {
+  try {
+   const {
+      name,
+      description,
+      price,
+      discountPrice,
+      countInStock,
+      category,
+      brand,
+      sizes,
+      colors,
+      collections,
+      materials,
+      gender,
+      images,
+      isFeatured,
+      isPublished,
+      tags,
+      dimensions,
+      weight,
+      sku,
+      } = req.body;
+
+      //Find product by ID
+      const product = await Product.findById(req.params.id);
+
+      if (product) {
+        product.name = name || product.name;
+        product.description = description || product.description;
+        product.price = price || product.price;
+        product.discountPrice = discountPrice || product.discountPrice;
+        product.countInStock = countInStock || product.countInStock;
+        product.category = category || product.category;
+        product.brand = brand || product.brand;
+        product.sizes = sizes || product.sizes;
+        product.colors = colors || product.colors;
+        product.collections = collections || product.collections;
+        product.materials = materials || product.materials;
+        product.gender = gender || product.gender;
+        product.images = images || product.images;
+          product.isFeatured =
+              isFeatured !== undefined ? isFeatured : product.isFeatured; // Update isFeatured only if a new value is provided|| product.isFeatured;
+        product.isPublished =
+            isPublished !== undefined ? isPublished : product.isPublished;
+        product.tags = tags || product.tags;
+        product.dimensions = dimensions || product.dimensions;
+        product.weight = weight || product.weight;
+        product.sku = sku || product.sku;
+
+        //   Save the updated product 
+        const updatedProduct = await product.save();
+        res.status(200).json(updatedProduct);
+      } else {
+        res.status(404).json({ message: "Product not found" });
+      }
+      
+
+
+
+  } catch (error) {
+    console.error(error)
+    res.status(500).json({ message: "Server error" });
+  }
+});
 module.exports = router;
